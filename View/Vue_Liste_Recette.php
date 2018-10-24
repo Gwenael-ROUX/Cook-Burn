6c56607c9dcb38b075c21c866d0ef13f9d4743d6
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2018-10-24 17:01:40 +0200

    modif vue liste recette

--- View/Vue_Liste_Recette.php
+++ View/Vue_Liste_Recette.php
@@ -1,2 +1,16 @@
 <h1>Liste des recettes</h1>
-
+<div>
+    <footer aria-label="Page navigation example">
+        <ul class="pagination justify-content-end">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
+    </footer>
+</div>
