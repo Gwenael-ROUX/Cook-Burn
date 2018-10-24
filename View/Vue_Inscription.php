1017b08119bf59991c986ded43bb39191f0a73a1
GONAND Simon <g17011956@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-24 17:13:50 +0200

    modif style inscription

--- View/Vue_Inscription.php
+++ View/Vue_Inscription.php
@@ -26,14 +26,10 @@ elseif ($step == 'SUCCESS'){
     <img class="mb-4" src="/View/bootstrap-4.1.3-dist/image/Logo.png" alt="Logo" width="100" height="100">
     <h1 class="h3 mb-3 font-weight-normal">Inscription d'un utilisateur</h1>
     <form id="form" class="form-signin" action="/Admin/inscription" method="post">
-
-        <label for="email" class="sr-only">Email address</label>
-        <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
-
-        <label for="pseudo" class="sr-only">Password</label>
-        <input type="text" id="pseudo" class="form-control" placeholder="Pseudo" required>
-        <br/>
-        <div class =text-center>
+        <div class="col-4 mx-auto">
+            <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus/>
+            <input type="text" id="pseudo" class="form-control" placeholder="Pseudo" required/>
+            <br/>
             <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="inscription"/>
             <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="annuler"/>
         </div>
