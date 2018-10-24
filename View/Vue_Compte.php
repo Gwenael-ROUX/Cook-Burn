781cfb19c7583b178d9dce532501632b8f31b189
GONAND Simon <g17011956@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-24 15:41:48 +0200

    Modification affichage + ajout formulaire de changement de mdp

--- View/Vue_Compte.php
+++ View/Vue_Compte.php
@@ -1,13 +1,23 @@
 <h1>Mon compte</h1>
 <div>
     <h2>Nom (pseudo) : </h2>
-    <?= $data['pseudo']?>
+    <?= $_SESSION['PSEUDO']?>
     <h2>Email : </h2>
-    <?= $data['email']?>
-    <h2>Mot de passe : </h2>
-    <?= $data['mdp']?>
-    <h2>Mes favoris : </h2>
-    <?= $data['favoris']?>
+    <?= $_SESSION['EMAIL']?>
+    <div class="text-center">
+        <form class="form-control" action="/Utilisateur/changePwd" method="post">
+            <label for="oldPwd">Email address</label>
+            <input type="text" id="oldPwd" class="form-control" placeholder="Ancien Mot de passe" required autofocus>
+
+            <label for="newPwd">Email address</label>
+            <input type="text" id="newPwd" class="form-control" placeholder="Nouveau Mot de passe" required autofocus>
+
+            <div class =text-center>
+                <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="Changer de mot de passe"/>
+                <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="Réinitialiser"/>
+            </div>
+        </form>
+    </div>
+    <h2>Mes Favoris : </h2><br/>
     <h2>Mes recettes : </h2>
-    <?= $data['mesrecettes']?>
 </div>
