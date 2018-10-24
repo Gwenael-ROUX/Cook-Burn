3ef6500bcf3623b730cb0ac7310e62f8aac18ba6
GONAND Simon <g17011956@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-24 17:18:36 +0200

    Correction name de l'input

--- View/Vue_Inscription.php
+++ View/Vue_Inscription.php
@@ -3,8 +3,8 @@
     <h1 class="h3 mb-3 font-weight-normal">Inscription d'un utilisateur</h1>
     <form id="form" class="form-signin" action="/Admin/inscription" method="post">
         <div class="col-4 mx-auto">
-            <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus/>
-            <input type="text" id="pseudo" class="form-control" placeholder="Pseudo" required/>
+            <input type="email" name="email" class="form-control" placeholder="Email address" required autofocus/>
+            <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required/>
             <br/>
             <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="inscription"/>
             <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="annuler"/>
