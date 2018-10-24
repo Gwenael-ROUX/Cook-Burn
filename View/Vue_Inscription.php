1ebab0681db388c2221fce8091932667cd302c6b
GONAND Simon <g17011956@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-24 15:28:47 +0200

    Changement style Inscription

--- View/Vue_Inscription.php
+++ View/Vue_Inscription.php
@@ -22,18 +22,20 @@ elseif ($step == 'SUCCESS'){
     $couleur = 'card-panel green';
 }
 ?>
-<img class="mb-4" src="bootstrap-4.1.3-dist/image/Logo.png" alt="" width="72" height="72">
-<h1 class="h3 mb-3 font-weight-normal">Inscription d'un utilisateur</h1>
-<form id="form" class="form-signin" action="/Admin/inscription" method="post">
+<div class="text-center">
+    <img class="mb-4" src="/View/bootstrap-4.1.3-dist/image/Logo.png" alt="Logo" width="100" height="100">
+    <h1 class="h3 mb-3 font-weight-normal">Inscription d'un utilisateur</h1>
+    <form id="form" class="form-signin" action="/Admin/inscription" method="post">
 
-                 <label for="email" class="sr-only">Email address</label>
-                 <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
+        <label for="email" class="sr-only">Email address</label>
+        <input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
 
-                 <label for="pseudo" class="sr-only">Password</label>
-                 <input type="text" id="pseudo" class="form-control" required>
-
-        <div>
-             <input class="btn btn-lg btn-primary btn-block" type="submit" name="action" value="inscription"/>
-             <input class="btn btn-lg btn-primary btn-block" type="reset" name="action" value="annuler"/>
+        <label for="pseudo" class="sr-only">Password</label>
+        <input type="text" id="pseudo" class="form-control" placeholder="Pseudo" required>
+        <br/>
+        <div class =text-center>
+            <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="inscription"/>
+            <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="annuler"/>
         </div>
-</form>
+    </form>
+</div>
