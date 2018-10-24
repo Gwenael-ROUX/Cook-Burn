6a8923e7d8dc555366e941ec8fb2e6477dcfb46b
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2018-10-24 17:00:21 +0200

    modif Recette

--- Controller/Recette.php
+++ Controller/Recette.php
@@ -27,37 +27,55 @@ class Recette
         require_once  __DIR__.'/../View/Vue_EndPage.php';
     }
 
+    public function listeRecette($param){
+        $data= [
+            'titrePage'=>'CreationRecette',
+        ];
+
+        require_once  __DIR__.'/../View/Vue_StartPage.php';
+
+        require_once  __DIR__.'/../View/Vue_Liste_Recette.php';
+
+        require_once  __DIR__.'/../View/Vue_EndPage.php';
+    }
+
+    public function showRecette (){
+
+    }
+
 
     public function ajouterRecette(){
-        $nom_r = filter_input(INPUT_POST,'nom_r');
-        $nb_conviv = filter_input(INPUT_POST,'nb_conviv');
-        $descr_c = filter_input(INPUT_POST,'descr_c');
-        $descr_l = filter_input(INPUT_POST,'descr_l');
+        $idu = $_SESSION['ID'];
+        $nomr = filter_input(INPUT_POST,'nomr');
+        $nbconviv = filter_input(INPUT_POST,'nbconviv');
+        $descrc = filter_input(INPUT_POST,'descrc');
+        $descrl = filter_input(INPUT_POST,'descrl');
         $etape = filter_input(INPUT_POST,'etape');
 
-        if (!empty($nom_r)){
+
+        if (empty($nomr)){
             header('Location: ../Controller/admin.php?step=NOM_R_I');
             exit();
         }
-        elseif (!empty($nb_conviv)){
+        elseif (empty($nbconviv)){
             header('Location: ../Controller/admin.php?step=NB_CONVIV_I');
             exit();
         }
-        elseif (!empty($descr_c)){
+        elseif (empty($descrc)){
             header('Location: ../Controller/admin.php?step=DESCR_C_I');
             exit();
         }
-        elseif (!empty($descr_l)){
+        elseif (empty($descrl)){
             header('Location: ../Controller/admin.php?step=DESCR_L_I');
             exit();
         }
-        elseif (!empty($etape)){
+        elseif (empty($etape)){
             header('Location: ../Controller/admin.php?step=ETAPE_I');
             exit();
         }
         else {
             $recette = new MRecette();
-            $recette->ajouterRecette($nom_r,$nb_conviv,$descr_c,$descr_l,$etape);
+            $recette->ajouterRecette($idu,$nomr,$nbconviv,$descrc,$descrl,$etape);
         }
     }
 
