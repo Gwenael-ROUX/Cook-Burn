1c784264f956297583eac9f49d9720051fc28e47
GONAND Simon <g17011956@a-maq-iutinfo.salsa.univ-amu.fr>
2018-10-24 16:30:38 +0200

    Sylisation changement mot de passe

--- View/Vue_Compte.php
+++ View/Vue_Compte.php
@@ -1,24 +1,21 @@
-<h1>Mon compte</h1>
+<h1 class="h1 mb-4 font-weight-bold">Mon compte</h1>
 <div>
-    <h2>Nom (pseudo) : </h2>
-    <?= $_SESSION['PSEUDO']?>
-    <h2>Email : </h2>
-    <?= $_SESSION['EMAIL']?>
-    <h2>Modification du Mot de passe : </h2>
-    <div class="text-center">
-        <form class="form-control" action="/Utilisateur/changePwd" method="post">
-            <label for="oldPwd" class="sr-only">Email address</label>
-            <input type="text" id="oldPwd" class="form-control" placeholder="Ancien Mot de passe" required autofocus>
-
-            <label for="newPwd" class="sr-only">Email address</label>
-            <input type="text" id="newPwd" class="form-control" placeholder="Nouveau Mot de passe" required autofocus>
-
-            <div class =text-center>
-                <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="Changer de mot de passe"/>
-                <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="Réinitialiser"/>
+    <p class=" mb-2 font-weight-normal">Pseudo : <?= $_SESSION['PSEUDO']?></p>
+    <h2 class="h4 mb-2 font-weight-normal">Email : </h2>
+    <p class=" mb-2 font-weight-normal"><?= $_SESSION['EMAIL']?></p>
+    <h2 class="h4 mb-2 font-weight-normal">Modification du Mot de passe : </h2>
+        <form action="/Utilisateur/changePwd" method="post">
+            <div class="container">
+                <div class="col-4 mx-auto">
+                    <input type="text" id="oldPwd" class="form-control" placeholder="Ancien Mot de passe" required autofocus>
+                    <input type="text" id="newPwd" class="form-control" placeholder="Nouveau Mot de passe" required>
+                    <input type="text" id="ConfirmNewPwd" class="form-control" placeholder="Confirmer mot de passe" required><br/>
+                    <input class="btn btn-lg btn-danger btn-block" type="submit" name="action" value="Changer de mot de passe"/>
+                    <input class="btn btn-lg btn-danger btn-block" type="reset" name="action" value="Réinitialiser"/>
+                </div>
             </div>
         </form>
-    </div>
-    <h2>Mes Favoris : </h2><br/>
-    <h2>Mes recettes : </h2>
+    <br/>
+    <h2 class="h4 mb-2 font-weight-normal">Mes Favoris : </h2><br/>
+    <h2 class="h4 mb-2 font-weight-normal">Mes recettes : </h2>
 </div>
