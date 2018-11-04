9186c84e5199f3476b8fa6e9e3dc7f506a59484b
Yvan Chauvy <yvan.chauvy@etu.univ-amu.fr>
2018-11-04 12:55:58 +0100

    Supression d'une vu dans le controller par erreur

--- Controller/Vue_Error.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title><?= $data['titrePage']?></title>
-</head>
-<body>
-    <h1><?= $data['titrePage']?></h1>
-    <p class="flow-text"><?= $data['text']?> </p>
-
