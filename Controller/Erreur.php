0906cdc4a198d347b7b00bc06b0892e23601988f
Yvan Chauvy <yvan.chauvy@etu.univ-amu.fr>
2018-11-04 13:01:25 +0100

    Ajout des commentaires de la classe

--- Controller/Erreur.php
+++ Controller/Erreur.php
@@ -8,19 +8,28 @@
 
 class Erreur
 {
-    public function index(){
-
-    }
+    /* Fonction qui traite l'erreur de la permission d'administrateru */
     public function pasAdmin (){
+        /* Definition du tableau comportant les informations necessaire aux vues */
         $data= [
             'titrePage'=>'Pas Admin',
             'text'=>'Veuillez vous connectez en tant qu\'administateur',
         ];
+        /* Affichage de la vue d'erreur */
         require_once __DIR__. '/../View/Vue_Error.php';
+        /* Affichage de la vue de fin de page */
         require_once __DIR__. '/../View/Vue_EndPage.php';
     }
+    /* Fonction qui traite l'erreur de la permission d'administrateru */
     public function erreur404(){
-        require_once __DIR__. '/../View/Vue_Error404.php';
+        /* Definition du tableau comportant les informations necessaire aux vues */
+        $data= [
+            'titrePage'=>'Erreur 404',
+            'text'=>'Page Introuvable',
+        ];
+        /* Affichage de la vue d'erreur */
+        require_once __DIR__. '/../View/Vue_Error.php';
+        /* Affichage de la vue de fin de page */
         require_once __DIR__. '/../View/Vue_EndPage.php';
     }
 }
